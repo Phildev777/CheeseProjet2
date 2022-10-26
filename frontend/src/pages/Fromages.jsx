5c43086f41d1687d3a89c03c5475b54b4a3deb54
AMELIE GUILLOUX <amelie.guilloux@hotmail.fr>
2022-10-26 12:16:06 +0200

    cleaned + deleted lint

--- frontend/src/pages/Fromages.jsx
+++ frontend/src/pages/Fromages.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const Fromages = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div>
+
+    </div>
+  );
 };
 
 export default Fromages;

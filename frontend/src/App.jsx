dad330d058c43bf1450458b89cfe368b8c9de485
AMELIE GUILLOUX <amelie.guilloux@hotmail.fr>
2022-10-26 12:03:52 +0200

    cleaned all

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,11 @@
-import Home from "./pages/Home";
+import React from "react";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Home />
-      <p>coucou</p>
+      <p>test</p>
     </div>
   );
 }

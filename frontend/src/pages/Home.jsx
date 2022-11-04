444c7d0266577a5f36b224226ab34baef561c7f6
AMELIE GUILLOUX <amelie.guilloux@hotmail.fr>
2022-11-04 19:48:34 +0100

    modified header and style

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import MobileTitle from "@components/MobileTitle";
 import Carousel from "../components/Carousel";
 import "../components/style/home.css";
 import Staff from "../components/Staff";
@@ -6,17 +7,17 @@ import Staff from "../components/Staff";
 function Home() {
   return (
     <div className="homepage">
-      <h1 className="header">Fromiton</h1>
+      <MobileTitle />
       <Carousel />
-      <p className="texthome">Qui sommes nous ?</p>
+      <h3 className="texthome">Qui sommes nous ?</h3>
       <Staff />
-      <p className="texthome">Nous conacter</p>
+      <h3 className="texthome">Nous contacter</h3>
       <p className="textcontact">
         Nous nous tenons à votre disposition pout toute suggestion ou demande de
         conseil.
       </p>
       <p className="textcontact">
-        Nous nous engageons à vous répondre dans les meilleurs deélais.
+        Nous nous engageons à vous répondre dans les meilleurs délais.
       </p>
     </div>
   );
